962a18460829a03b06ab04db3ce2b36b8c628f30
anish <anish.dubey@citymall.live>
2022-02-12 08:38:51 +0530

    init

--- app/src/main/java/com/anishdubey/blinkeffectexample/MainActivity.kt
+++ app/src/main/java/com/anishdubey/blinkeffectexample/MainActivity.kt
@@ -2,10 +2,21 @@ package com.anishdubey.blinkeffectexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.anishdubey.blinklibrary.BlinkEffect
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn = findViewById(R.id.buttonView)
+        btn.setOnClickListener {
+            // use of blink-library
+            BlinkEffect.blink(btn)
+        }
     }
 }
